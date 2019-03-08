cf8d2f776c7a4856c76873ce719ddd744ce48033
Prannav Gupta <prannavgupta23@outlook.com>
2019-03-07 19:36:35 -0600

    added code for 2D live animation

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request
+import numpy as np
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -18,4 +20,18 @@ def api():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
+plt.axis([0, 10, 0, 1])
+
+"""
+Sample code for plotting. 
+Tested. Works.
+Taken from: https://stackoverflow.com/questions/11874767/how-do-i-plot-in-real-time-in-a-while-loop-using-matplotlib
+plt.axis([0, 10, 0, 1])
+for i in range(10):
+    y = np.random.random()
+    plt.scatter(i, y)
+    plt.pause(0.05)
+
+plt.show()
+"""
 
