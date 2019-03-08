327dc0e0b710f13bad696583183f320373421bce
Prannav Gupta <prannavgupta23@outlook.com>
2019-03-07 19:21:11 -0600

    handled POST requests

--- server/app.py
+++ server/app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     print("All good!")    
-    return "200"
+    return 200
+
+@app.route('/api/', methods = ['POST','GET'])
+def api():
+    if request.method == "POST":
+        sample_var = request.form["base"]
+        return sample_var
+    
+    return 400
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
 
